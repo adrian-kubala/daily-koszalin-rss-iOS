c6626d7d3af8857976d73cac0ab3d052e9ef0205
Adrian <adrian.kubala@nextisgreat.com>
2016-08-19 15:28:47 +0200

    Initial commit. Made four classes.

---
+++ Daily Koszalin/Daily Koszalin/XMLParser.swift
@@ -0,0 +1,13 @@
+//
+//  XMLParser.swift
+//  Daily Koszalin
+//
+//  Created by Adrian on 19.08.2016.
+//  Copyright © 2016 Adrian Kubała. All rights reserved.
+//
+
+import UIKit
+
+class XMLParser: NSObject {
+
+}
