2807e6c016ad51d81b2a51c010990f76aa15ea6a
Adrian <adrian.kubala@nextisgreat.com>
2016-08-23 01:05:20 +0200

    Configured ContainerViewController class (set it as rootVC in AppDelegate and overrided TraitCollection)

--- Daily Koszalin/Daily Koszalin/ContainerViewController.swift
+++ Daily Koszalin/Daily Koszalin/ContainerViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 class ContainerViewController: UIViewController {
+    
+    var viewController: UISplitViewController!
+    
 
+    func setEmbeddedViewController(splitViewController: UISplitViewController!) {
+        if splitViewController != nil {
+            viewController = splitViewController
+            
+            addChildViewController(viewController)
+            view.addSubview(viewController.view)
+            viewController.didMoveToParentViewController(self)
+            
+            self.setOverrideTraitCollection(UITraitCollection.init(horizontalSizeClass: UIUserInterfaceSizeClass.Regular), forChildViewController: viewController)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +35,4 @@ class ContainerViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
