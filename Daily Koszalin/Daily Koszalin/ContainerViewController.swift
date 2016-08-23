f7f9ed963051315e2927f4406636ae65930ba29b
Adrian <adrian.kubala@nextisgreat.com>
2016-08-23 21:21:00 +0200

    Showing single VC in the portrait on smaller iPhones and collapsed secondary VC onto primary one.

--- Daily Koszalin/Daily Koszalin/ContainerViewController.swift
+++ Daily Koszalin/Daily Koszalin/ContainerViewController.swift
@@ -17,12 +17,22 @@ class ContainerViewController: UIViewController {
         if splitViewController != nil {
             viewController = splitViewController
             
-            addChildViewController(viewController)
-            view.addSubview(viewController.view)
+            self.addChildViewController(viewController)
+            self.view.addSubview(viewController.view)
             viewController.didMoveToParentViewController(self)
             
-            self.setOverrideTraitCollection(UITraitCollection.init(horizontalSizeClass: UIUserInterfaceSizeClass.Regular), forChildViewController: viewController)
+            //self.setOverrideTraitCollection(UITraitCollection.init(horizontalSizeClass: UIUserInterfaceSizeClass.Regular), forChildViewController: viewController)
         }
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        if size.width > size.height {
+            self.setOverrideTraitCollection(UITraitCollection.init(horizontalSizeClass: UIUserInterfaceSizeClass.Regular), forChildViewController: viewController)
+        } else {
+            self.setOverrideTraitCollection(nil, forChildViewController: viewController)
+        }
+        
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
     }
     
     override func viewDidLoad() {
