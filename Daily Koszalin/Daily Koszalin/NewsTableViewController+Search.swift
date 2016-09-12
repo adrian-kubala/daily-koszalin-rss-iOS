19b17bf7675184a71f684a501ca7e6af0c531ad6
Adrian <adrian.kubala@nextisgreat.com>
2016-09-12 20:08:51 +0200

    Filtering by dates

---
+++ Daily Koszalin/Daily Koszalin/NewsTableViewController+Search.swift
@@ -0,0 +1,9 @@
+//
+//  NewsTableViewController+Search.swift
+//  Daily Koszalin
+//
+//  Created by Adrian on 12.09.2016.
+//  Copyright © 2016 Adrian Kubała. All rights reserved.
+//
+
+import Foundation
