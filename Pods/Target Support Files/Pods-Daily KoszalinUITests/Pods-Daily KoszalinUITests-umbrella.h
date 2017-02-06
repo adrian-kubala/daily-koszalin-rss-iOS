997ed0e4b1c4ca2db525f4b2b89924b67d7abec0
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-06 13:42:34 +0100

    Delete tests targets

--- Pods/Target Support Files/Pods-Daily KoszalinUITests/Pods-Daily KoszalinUITests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Daily_KoszalinUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Daily_KoszalinUITestsVersionString[];
-
