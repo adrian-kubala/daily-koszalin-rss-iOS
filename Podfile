5bdee950d74ffff06017589fc901f91479606fe7
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-06 18:07:00 +0100

    Rename project

--- Podfile
+++ Podfile
@@ -2,7 +2,7 @@
 # platform :ios, '9.0'
 use_frameworks!
 
-target 'Daily Koszalin' do
+target 'InfoKoszalin' do
   pod 'FeedKit', '~> 5.1.0'
   pod 'AlamofireImage', '~> 3.2.0'
   pod 'RealmSwift', '~> 2.4.2'
--- Pods/Target Support Files/Pods-InfoKoszalin/Pods-InfoKoszalin-umbrella.h
+++ Pods/Target Support Files/Pods-InfoKoszalin/Pods-InfoKoszalin-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_Daily_KoszalinVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Daily_KoszalinVersionString[];
+FOUNDATION_EXPORT double Pods_InfoKoszalinVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_InfoKoszalinVersionString[];
 
