6a4435c4473a53a2b986d8ea84cb1f2451aabf38
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-23 14:40:20 +0100

    Add NotificationCenter extension

---
+++ InfoKoszalin/NotificationCenter+Observing.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationCenter+observe.swift
+//  CloudPlayer
+//
+//  Created by Adrian Kubała on 27.01.2017.
+//  Copyright © 2017 Adrian Kubała. All rights reserved.
+//
+
+import Foundation
+
+extension NotificationCenter {
+  func observeNotification(withName name: Notification.Name?, block: @escaping (Notification) -> ()) {
+    addObserver(forName: notificationName, object: nil, queue: nil, using: block)
+  }
+}
